6c7ea9a1d16a76193f4001333464081d5215d6fa
chimmyJay <chimmyJay@outlook.com>
2022-03-22 14:48:45 +0800

    comment

--- CodeSmell/Program.cs
+++ CodeSmell/Program.cs
@@ -29,33 +29,53 @@ namespace CodeSmell
         {
             Console.WriteLine("------------------------------------------");
 
-            //Print Subscription Type
-            Console.WriteLine($@"Subscription type: '{order.SubscriptionType}' ");
+            //PrintSubscriptionType
+            PrintSubscriptionType(order);
 
             Console.WriteLine("------------------------------------------");
 
-            //Print Each Product Price
+            //PrintEachProductPrice
+            PrintEachProductPrice(order);
+
+            Console.WriteLine("------------------------------------------");
+
+            //PrintTotalPrice
+            PrintTotalPrice(order);
+
+            Console.WriteLine("------------------------------------------");
+
+            //PrintLicenseeInformation
+            PrintLicenseeInformation(order);
+
+            Console.WriteLine("------------------------------------------");
+        }
+
+        private static void PrintLicenseeInformation(JetBrainsOrder order)
+        {
+            Console.WriteLine("Licensee information");
+            Console.WriteLine($"UserName : {order.UserName}");
+            Console.WriteLine($"Email : {order.UserEmail}");
+        }
+
+        private static void PrintTotalPrice(JetBrainsOrder order)
+        {
+            Console.WriteLine("{0,30}", $"Total Price    {order.GetTotalPrice()} USD");
+            Console.WriteLine("{0,30}", $"Tax(5%)   {GetTax(order)} USD");
+            Console.WriteLine("{0,30}", $"Grand Total   {GetGrandTotal(order)} USD");
+        }
+
+        private static void PrintEachProductPrice(JetBrainsOrder order)
+        {
             Console.WriteLine("Product         || Item Price ");
             foreach (var product in order.Products)
             {
                 Console.WriteLine("{0,-16}|| {1,6} USD  ", product.Name, product.Price);
             }
+        }
 
-            Console.WriteLine("------------------------------------------");
-
-            //Print Total Price
-            Console.WriteLine("{0,30}", $"Total Price    {order.GetTotalPrice()} USD");
-            Console.WriteLine("{0,30}", $"Tax(5%)   {GetTax(order)} USD");
-            Console.WriteLine("{0,30}", $"Grand Total   {GetGrandTotal(order)} USD");
-
-            Console.WriteLine("------------------------------------------");
-
-            //Print Licensee Information
-            Console.WriteLine("Licensee information");
-            Console.WriteLine($"UserName : {order.UserName}");
-            Console.WriteLine($"Email : {order.UserEmail}");
-
-            Console.WriteLine("------------------------------------------");
+        private static void PrintSubscriptionType(JetBrainsOrder order)
+        {
+            Console.WriteLine($@"Subscription type: '{order.SubscriptionType}' ");
         }
 
         private static decimal GetTax(JetBrainsOrder order)
